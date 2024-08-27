cc104f8497641a7d74870316bd87992ff26b4fb6
Anna-277 <yuava@inbox.ru>
2024-08-27 23:57:49 +0500

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+homework_completed = 12
+hours_spent = 1.5
+name_of_cours = "Python"
+time_for_one_task = hours_spent/homework_completed
+print("Курс:",name_of_cours,"," ,"всего задач:",homework_completed, ",", "затрачено часов:",hours_spent, ",", "среднее время выполнения",time_for_one_task,"часа")
